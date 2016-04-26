993a32fb0086740eae311fc02928126e2c6c16cf
nishant-cyro <nishant.tuteja@vinsol.com>
2016-04-26 13:24:32 +0530

    Modify load_data_and_set_original_file_as_attachment in product_import_mailer

--- app/mailers/spree/product_import_mailer.rb
+++ app/mailers/spree/product_import_mailer.rb
@@ -16,7 +16,7 @@ class Spree::ProductImportMailer < Spree::BaseMailer
     def load_data_and_set_original_file_as_attachment(import_product_id, file_field)
       @import_product = Spree::ProductImport.find(import_product_id)
       original_file_name = @import_product.send("#{ file_field }_file_name")
-      original_file_path = @import_product.send(file_field).path
+      original_file_path = @import_product.send("#{ file_field }_path")
       attachments[original_file_name] = File.read(original_file_path)
     end
 end
